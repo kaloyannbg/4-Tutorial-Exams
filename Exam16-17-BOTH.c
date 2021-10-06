410798304dd080daaaab0b1f2b2ac7f9b5b3ae7f
kaloyannbg <75050098+kaloyannbg@users.noreply.github.com>
2021-10-07 02:32:24 +0300

    Create Exam16-17-BOTH.c

---
+++ Exam16-17-BOTH.c
@@ -0,0 +1,51 @@
+/* Задача 16. Да се дефинира константата Пи (3.14159...) и да се напише
+функция, която изчислява лицето на окръжност по даден радиус (Пи * R * R).
+В main() да се извика горната функция с радиуси 1, 1.5, 13 */
+
+/*
+
+Задача 17.* Към задача 16 да се добави втора функция, която изчислява
+лицето на елипса по зададени два параметъра ( Пи * A * B)
+
+*/
+
+#include <stdio.h>
+
+double circleArea(double radius);
+double ellipseArea(double radiusA, double radiusB);
+
+int main() {
+
+    double radiusOne = 1.0;
+    double radiusTwo = 1.5;
+    double radiusThree = 13.0;
+
+
+    printf("Circle Area with radius: %.2f is: %f\n", radiusOne, circleArea(radiusOne) );
+    printf("Circle Area with radius: %.2f is: %f\n", radiusTwo, circleArea(radiusTwo) );
+    printf("Circle Area with radius: %.2f is: %f\n", radiusThree, circleArea(radiusThree) );
+
+
+    printf("Ellipse Area with radiusA: %.2f and radiusB: %.2f is: %f\n", radiusOne, radiusTwo, ellipseArea(radiusOne, radiusTwo) );
+
+}
+
+double circleArea(double radius) {
+
+    const double PI = 3.14159;
+
+    double area = PI * radius * radius;
+
+    return area;
+
+}
+
+double ellipseArea(double radiusA, double radiusB) {
+
+    const double PI = 3.14159;
+
+    double area = PI * radiusA * radiusB;
+
+    return area;
+
+}
